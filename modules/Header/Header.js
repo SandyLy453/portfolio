bab966537cf45d3ddfafb9be16eda9f35c8d122a
Boi San Ly <boisanly@Mac.lan>
2025-01-10 09:05:07 -0800

    Add components

---
+++ modules/Header/Header.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from './Header.module.css'
+
+export default function Header () {
+    return (
+        <header className={styles.header}>
+            <div className={styles.logo}>
+                <Image src={'/logo.png'} alt="logo" className={styles.logo} width={100} height={37.5} />
+            </div>
+
+            <div className={styles.menu}>
+                <p className={styles.menuOption}><Link href={'/'} className={styles.link}>Project</Link></p>
+                <p className={styles.menuOption}><Link href={'/'} className={styles.link}>About me</Link></p>
+            </div>
+        </header>
+    )
+}
