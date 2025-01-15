02dfa1107d1e6300ae04b93ed63eaad4b5876104
Boi San Ly <boisanly@Mac.lan>
2025-01-14 22:47:37 -0800

    Update meta info

--- .history/pages/_document_20250110085956.js
+++ .history/pages/_document_20250110085956.js
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>San San's Tea Room</title>
-        <link rel="icon" href="/favicon.ico" /> {/* Add favicon */}
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
