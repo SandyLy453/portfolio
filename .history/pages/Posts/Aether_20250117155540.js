783cb29eea85708605548ebca93c0d522282441e
Boi San Ly <boisanly@Mac.lan>
2025-01-17 20:29:08 -0800

    adding photo and update aether page

---
+++ .history/pages/Posts/Aether_20250117155540.js
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import Image from "next/image";
+import Header from "@/modules/Header/Header";
+import Footer from "@/modules/Footer/Footer";
+import PostHeader from "@/modules/PostHeader/PostHeader";
+import styles from '@/styles/Aether.module.css'
+
+export default function Aether() {
+    return (
+        <>
+            <Header />
+
+            <main className={styles.body}>
+                <PostHeader 
+                    title="Aether - Saving time for what matter the most"
+                    date="By Aether Team | 2024"
+                />
+
+            <div className={styles.intro}>
+                <Image src={'/aether.gif'} alt="aether logo" className={styles.gifOne} width={144*4} height={102*4}/>
+                <div className={styles.text}>
+                    <h2 className="subHeading">
+                        Timeline
+                    </h2>
+                    <p className={styles.content}>
+                        September 2024 - December 2024
+                    </p>
+                </div>
+            </div>
+
+            <div className={styles.sectionBreaker}>
+                <span> Problem </span>
+            </div>
+
+            <div className={styles.middle}>
+                <div className={styles.text}>
+                    <h2 className="subHeading">
+                        How did we solve this?
+                    </h2>
+                    <p className={styles.content}>
+                        To solve this problem, we created <span className={styles.span}>Aether</span>. With <span className={styles.span}>Aether</span>, filling out forms becomes easier, faster, and significantly less stressful.
+                    </p>
+                </div>
+                <Image src={'/aether.gif'} alt="aether logo" className={styles.gifOne} width={144*4} height={102*4}/>
+            </div>
+
+            </main>
+
+            <Footer />
+        </>
+    )
+}
+
---
+++ .history/pages/Posts/Aether_20250117163759.js
@@ -0,0 +1,105 @@
+import { useState } from "react";
+import Image from "next/image";
+import Header from "@/modules/Header/Header";
+import Footer from "@/modules/Footer/Footer";
+import PostHeader from "@/modules/PostHeader/PostHeader";
+import styles from '@/styles/Aether.module.css'
+
+export default function Aether() {
+
+    const personaImages = [
+        { src: "/persona1.png", alt: "Persona 1" },
+        { src: "/persona2.png", alt: "Persona 2" },
+    ];
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) =>
+        prevIndex === personaImages.length - 1 ? 0 : prevIndex + 1
+        );
+    };
+
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? personaImages.length - 1 : prevIndex - 1
+        );
+    };
+
+    return (
+        <>
+            <Header />
+
+            <main className={styles.body}>
+                <PostHeader 
+                    title="Aether - Saving time for what matter the most"
+                    date="By Aether Team | 2024"
+                />
+
+            <div className={styles.intro}>
+                <Image src={'/aether.gif'} alt="aether logo" className={styles.gifOne} width={144*4} height={102*4}/>
+                <div className={styles.text}>
+                    <h2 className={styles.subHeading}>
+                        Timeline
+                    </h2>
+                    <p className={styles.content}>
+                        September 2024 - December 2024
+                    </p>
+                    <h2 className={styles.subHeading}>
+                        Role
+                    </h2>
+                    <p className={styles.content}>
+                        UX/UI Designer | Front-end Developer | Researcher
+                    </p>
+                    <h2 className={styles.subHeading}>
+                        Tools
+                    </h2>
+                    <p className={styles.content}>
+                        Figma, NextJS, React
+                    </p>
+                </div>
+            </div>
+
+            <div className={styles.sectionBreaker}>
+                <span> Problem </span>
+            </div>
+
+            <div className={styles.middle}>
+                <div className={styles.text}>
+                    <h2 className="subHeading">
+                        What was the problem?
+                    </h2>
+                    <p className={styles.content}>
+                        We discovered that completing paperwork can take an individual up to <span className={styles.span}>30 days</span> a year, and for caregivers, it often takes even longer. Most forms are highly stressful and challenging to fill out due to their complexity, lengthy content, and occasionally unclear instructions.
+                    </p>
+                </div>
+
+                <div className={styles.text}>
+                    <h2 className="subHeading">
+                        How did we solve this?
+                    </h2>
+                    <p className={styles.content}>
+                        To solve this problem, we created <span className={styles.span}>Aether</span>. With <span className={styles.span}>Aether</span>, filling out forms becomes easier, faster, and significantly less stressful.
+                    </p>
+                </div>
+            </div>
+
+            <div className={styles.infoSection}>
+                <h2 className="subHeading">
+                    Persona
+                </h2>
+
+                <div className={styles.photoSlides}>
+                    <Image src={'/persona1.png'} alt="aether logo" className={styles.persona} width={850/1.5} height={1100/1.5}/>
+                    <Image src={'/persona2.png'} alt="aether logo" className={styles.persona} width={850/1.5} height={1100/1.5}/>
+                </div>
+
+            </div>
+
+            </main>
+
+            <Footer />
+        </>
+    )
+}
+
